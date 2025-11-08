754b95f69509ea57bd7d92e11ce4b847b6590b68
mvusic07 <thinkman405@gmail.com>
2025-11-08 18:57:32 +0100

    2nd

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/imim77/crypto-indicator
 
 go 1.25.2
+
+require (
+	github.com/VictorLowther/btree v0.1.6 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/VictorLowther/btree"
+	"github.com/gorilla/websocket"
+)
+
+const wsendpoint = "wss://fstream.binance.com/stream?streams=btcusdt@depth"
+
+func byBestBid(a, b *OrderbookEntry) bool {
+	return a.Price >= b.Price
+}
+
+func byBestAsk(a, b *OrderbookEntry) bool {
+	return a.Price < b.Price
+}
+
+type OrderbookEntry struct {
+	Price  float64
+	Volume float64
+}
+
+type Orderbook struct {
+	Asks *btree.Tree[*OrderbookEntry]
+	Bids *btree.Tree[*OrderbookEntry]
+}
+
+func (ob *Orderbook) handleDepthResponse(res BinanceDepthResult) {
+	for _, ask := range res.Asks {
+		price, _ := strconv.ParseFloat(ask[0], 64)
+		volume, _ := strconv.ParseFloat(ask[1], 64)
+		if volume == 0 {
+			return
+		}
+		entry := &OrderbookEntry{
+			Price:  price,
+			Volume: volume,
+		}
+		ob.Asks.Insert(entry)
+	}
+	for _, bid := range res.Bids {
+		price, _ := strconv.ParseFloat(bid[0], 64)
+		volume, _ := strconv.ParseFloat(bid[1], 64)
+		if volume == 0 {
+			return
+		}
+		entry := &OrderbookEntry{
+			Price:  price,
+			Volume: volume,
+		}
+		ob.Bids.Insert(entry)
+	}
+}
+
+func NewOrderBook() *Orderbook {
+	return &Orderbook{
+		Asks: btree.New(byBestAsk),
+		Bids: btree.New(byBestBid),
+	}
+}
+
+type BinanceDepthResult struct {
+	Asks [][]string `json:"a"`
+	Bids [][]string `json:"b"`
+}
+
+type BinanceDepthResponse struct {
+	Stream string             `json:"stream"`
+	Data   BinanceDepthResult `json:"data"`
+}
 
 func main() {
-	fmt.Println("hello")
+	conn, _, err := websocket.DefaultDialer.Dial(wsendpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		ob     = NewOrderBook()
+		result BinanceDepthResponse
+	)
+
+	for {
+		if err := conn.ReadJSON(&result); err != nil {
+			log.Fatal(err)
+		}
+		ob.handleDepthResponse(result.Data)
+		it := ob.Asks.Iterator(nil, nil)
+		for it.Next() {
+			fmt.Printf("%+v\n", it.Item())
+		}
+	}
+
 }
